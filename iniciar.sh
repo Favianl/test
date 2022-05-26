a3953dda53f32d1ff526c7060fbb911b9656efeb
favian <leofavianf@gmail.com>
2022-05-26 18:08:49 +0000

    modificando lightdm

--- iniciar.sh
+++ iniciar.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox --noconfirm
+clear
 cp -r .config/bspwm ~/.config
 cp -r .config/sxhkd ~/.config
 cp .xprofile ~/
-#sudo systemctl enable lightdm
-#sudo systemctl start lightdm
+clear
+sudo systemctl enable lightdm
+sudo systemctl start lightdm
 echo "==========="
 echo "Terminado"
 echo "==========="
