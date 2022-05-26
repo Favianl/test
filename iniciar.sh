030a80f08e67c5daf7aa3e1eeec483447f76c7b1
favian <leofavianf@gmail.com>
2022-05-26 18:41:11 +0000

    agregando yay

--- iniciar.sh
+++ iniciar.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox --noconfirm
+pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox base-devel --noconfirm
 clear
 cp -r .config/bspwm ~/.config
 cp -r .config/sxhkd ~/.config
 cp .xprofile ~/
-clear
+cd /opt
+git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER ./yay-git
+cd yay-git/
+makepkg -si
 sudo systemctl enable lightdm
-sudo systemctl start lightdm
 echo "==========="
 echo "Terminado"
 echo "==========="
