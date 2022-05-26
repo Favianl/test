07ed87b83c2cc306c77f0d3b67da87fc7668e31a
favian <leofavianf@gmail.com>
2022-05-26 13:48:02 +0000

    third test...

--- iniciar.sh
+++ iniciar.sh
@@ -1,4 +1,14 @@
-#pacman -S bspwm tilix rofi sxhkd pcmanfm neofetch htop gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox --noconfirm
+#!/bin/bash
+pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox --noconfirm
+cp -r .config/bspwm ~/.config
+cp -r .config/sxhkd ~/.config
+cp .xprofile ~/
+systemctl enable lightdm
+systemctl start lightdm
+echo "==========="
+echo "Terminado"
+echo "==========="
+
 
 
  
