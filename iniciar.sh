bf0496e6c9df071409d9b2617abd81765e6aadb1
favian <leofavianf@gmail.com>
2022-05-26 13:18:51 +0000

    second test...

---
+++ iniciar.sh
@@ -0,0 +1,5 @@
+#pacman -S bspwm tilix rofi sxhkd pcmanfm neofetch htop gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox --noconfirm
+
+
+ 
+
