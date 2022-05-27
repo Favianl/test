9392f81dfef59a927f2fb1322bda7bbc453d0498
favian <leofavianf@gmail.com>
2022-05-27 12:16:30 +0000

    test nuevo vm...

--- iniciar.sh
+++ iniciar.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox base-devel --noconfirm
+pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop xf86-video-vesa xf86-video-ati xorg-server xorg-xinit mesa mesa-demos --noconfirm
+#gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox base-devel --noconfirm
 clear
 cp -r .config/bspwm ~/.config
+echo "copiando bspwm..."
 cp -r .config/sxhkd ~/.config
-cp -r .config/polybar ~/.config
-sudo chmod +x ~/.config/polybar/launch.sh
+echo "copiando sxhkd..."
+#cp -r .config/polybar ~/.config
+#sudo chmod +x ~/.config/polybar/launch.sh
 cp .xprofile ~/
+echo "copiando xprofile..."
 #git clone https://aur.archlinux.org/yay-git.git
 #sudo chown -R $USER:$USER yay-git
 #cd yay-git/
