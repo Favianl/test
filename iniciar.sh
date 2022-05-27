828ad8146fd422984e5aac54dd86e2fdd148d75d
favian <leofavianf@gmail.com>
2022-05-27 13:28:09 +0000

    final beta...

--- iniciar.sh
+++ iniciar.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop xf86-video-vesa xf86-video-ati xorg-server xorg-xinit mesa mesa-demos --noconfirm
-#gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox base-devel --noconfirm
+pacman -S bspwm tilix rofi sxhkd pcmanfm lightdm lightdm-gtk-greeter neofetch htop xf86-video-vesa xf86-video-ati xorg-server xorg-xinit mesa mesa-demos gvfs leafpad picom nitrogen lxappearance arandr neovim gnome-calculator lsd bat vlc pulseaudio pulseaudio-alsa pavucontrol gpicview evince network-manager-applet firefox base-devel --noconfirm
 clear
 cp -r .config/bspwm ~/.config
 echo "copiando bspwm..."
 cp -r .config/sxhkd ~/.config
 echo "copiando sxhkd..."
 cp -r .config/polybar ~/.config
-echo "copiando sxhkd..."
+echo "copiando polybar..."
 chmod +x ~/.config/polybar/launch.sh
 echo "cambiando permiso a launch..."
 cp .xprofile ~/
 echo "copiando xprofile..."
 git clone https://aur.archlinux.org/yay-git.git
 echo "clonando yay..."
-#sudo chown -R $USER:$USER yay-git
-#cd yay-git/
-#makepkg -si --noconfirm
-#sudo yay -S polybar
+sudo chown -R $USER:$USER yay-git
+cd yay-git/
+echo "compilando yay"
+makepkg -si --noconfirm
+echo "instalando polybar"
+sudo yay -S polybar --noconfirm
+echo "habilitando lightdm (iniciar sesion)"
 sudo systemctl enable lightdm
-echo "======Instalando Polybar======="
-echo "1 - sudo chown -R $USER:$USER yay-git"
-echo "2 - cd yay-git"
-echo "3 - makepkg -si"
-echo "4 - sudo yay -S polybar"
+echo "cambiando idioma teclado..."
+sudo set-x11-keymap es
 echo "============FIN==============="
 
 
