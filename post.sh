f2713a5eb49cee77df6793b56e8e75e522a324b1
favian <leofavianf@gmail.com>
2022-06-20 12:19:38 -0500

    modificando instalaciòn

--- post.sh
+++ post.sh
@@ -5,6 +5,6 @@ nvm install v18.3.0
 sleep 3
 nvm use v16.15.1 
 sudo pacman -S npm --noconfirm
-sudo npm install -g prettier
+sudo npm install -g prettier live-server
 yay -S brave-bin --noconfirm
 nvim +PackerSync
